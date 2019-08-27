5e7d1ad5ff89deb13aabc41c7ead80510416249a
Karol Skolasiński <karolskolasinski@gmail.com>
2019-08-27 15:53:51 +0200

    parser

---
+++ src/main/java/demo/Student.java
@@ -0,0 +1,16 @@
+package demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private int age;
+    private Double average;
+    private boolean alive;
+}
